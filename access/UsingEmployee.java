d2de8f3e77c146a1e8defaf30ed77a2b480aaa04
Deepak M T <deepakmakkatty@gmail.com>
2022-10-18 21:56:32 +0530

    Access Employee Example program

---
+++ access/UsingEmployee.java
@@ -0,0 +1,17 @@
+package com.nc.training.access;
+
+public class UsingEmployee {
+
+    public static void main(String[] args) {
+        // Employee employee = new Employee();
+
+        final int x = 10;
+        // x = 15;
+
+        Employee.display();
+
+        System.out.println(Employee.getRetirementAge());
+
+
+    }
+}
